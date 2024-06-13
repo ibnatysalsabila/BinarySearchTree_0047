f2bbb1a369ca2219d758b98ddd0404f8a4f04509
ibnatysalsabila <salsajpr01@gmail.com>
2024-06-13 20:13:37 +0700

    memastikan bahwa jika parent = null dan tandai node baru sebagai root

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -36,5 +36,10 @@ public:
 		Node* parent = NULL;
 		Node* currentNode = NULL;
 		search(element, parent, currentNode); // Locate the node which be the parent of the node to be inserted
-
+		
+		if (parent == NULL) // If the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode; // Mark the new node as ROOT
+			return; // Exit
+		}
 		
